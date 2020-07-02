a8afa4f96901ac7c1a0baae3311d790f7cbcc178
BorisP1996 <prposboris13@hotmail.com>
2020-07-02 13:52:14 +0200

    CreaetManagerWorks

--- Dan_XLIII_Boris_Prpos/Zadatak_1/ViewModel/CreateManagerViewModel.cs
+++ Dan_XLIII_Boris_Prpos/Zadatak_1/ViewModel/CreateManagerViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using Zadatak_1.Command;
 using Zadatak_1.Model;
@@ -20,6 +22,9 @@ namespace Zadatak_1.ViewModel
             createManagerWindow = cmOpen;
             SectorList = GetSectors();
             LevelList = GetLevels();
+            Employe = new tblEmploye();
+            Sector = new tblSector();
+            Level = new tblLevel();
         }
         #region Properties
         private tblEmploye employe;
@@ -103,11 +108,65 @@ namespace Zadatak_1.ViewModel
         }
         private void SaveExecute()
         {
+            try
+            {
+                tblEmploye newEmploye = new tblEmploye();
+                newEmploye.FirstName = Employe.FirstName;
+                newEmploye.Surname = Employe.Surname;
+                newEmploye.JMBG = Employe.JMBG;
+                newEmploye.Salary = Convert.ToInt32(Employe.Salary);
+                newEmploye.Account = Employe.Account;
+                newEmploye.Pasword = Employe.Pasword;
+                newEmploye.Username = Employe.Username;
+                newEmploye.Position = Employe.Position;
+                newEmploye.Email = Employe.Email;
+                string birthdate = CalculateBirth(Employe.JMBG);
+
+                newEmploye.DateOfBirth = DateTime.ParseExact(CalculateBirth(Employe.JMBG),"yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+        
+
+                context.tblEmployes.Add(newEmploye);
+                context.SaveChanges();
+                tblManager newManager = new tblManager();
+
+                newManager.EmployeID = newEmploye.EmployeID;
+                newManager.SectorID = Sector.SectorID;
+                newManager.LevelID = Level.LevelID;
+
+                context.tblManagers.Add(newManager);
+
+                context.SaveChanges();
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+            {
+                Exception raise = dbEx;
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        string message = string.Format("{0}:{1}",
+                            validationErrors.Entry.Entity.ToString(),
+                            validationError.ErrorMessage);
+                        // raise a new exception nesting  
+                        // the current instance as InnerException 
+                        MessageBox.Show(message);
+                        raise = new InvalidOperationException(message, raise);
+                    }
+                }
+                throw raise;
+            }
 
         }
         private bool CanSaveExecute()
         {
-            return false;
+            if (String.IsNullOrEmpty(Employe.FirstName) || String.IsNullOrEmpty(Employe.Surname) || String.IsNullOrEmpty(Employe.JMBG) || Employe.JMBG.Length<13 || String.IsNullOrEmpty(Employe.Account)||String.IsNullOrEmpty(Employe.Salary.ToString()) || String.IsNullOrEmpty(Employe.Position) || String.IsNullOrEmpty(Employe.Username) || String.IsNullOrEmpty(Employe.Pasword) || String.IsNullOrEmpty(Employe.Email) || String.IsNullOrEmpty(Sector.SectorName) || String.IsNullOrEmpty(Level.LevelType))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
         private ICommand close;
         public ICommand Close
@@ -141,6 +200,26 @@ namespace Zadatak_1.ViewModel
             List<tblLevel> listOfLevels = new List<tblLevel>();
             listOfLevels = context.tblLevels.ToList();
             return listOfLevels;
+        }
+        public string CalculateBirth(string jmbg)
+        {
+            string needed = jmbg.Substring(0, 7);
+            int milenium = Convert.ToInt32(needed.Substring(5, 1));
+            int god = 0;
+            if (milenium == 0)
+            {
+                god = 2;
+            }
+            else
+            {
+                god = 1;
+            }
+            string year = god + needed.Substring(4, 3);
+            string month = needed.Substring(2, 2);
+            string day = needed.Substring(0, 2);
+
+            string complete = year + "-" + month + "-" + day;
+            return complete;
         }
         #endregion
     }
